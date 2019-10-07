c977df223fff01a19b0ea06ccb32c883df38ad28
Jerry Fernandez <gmfernandez@cpp.edu>
2019-10-06 23:39:34 -0700

    Basic methods implemented.

--- Assignment3.py
+++ Assignment3.py
@@ -1,9 +1,14 @@
 import sys
 import math
 
-eps = sys.float_info.epsilon
+epsilon = sys.float_info.epsilon #10**(-7) #= 10^(-7) %7 digit accuracy is desired 
+delta = 10**(-14) # Don't want to divide by a number smaller than this
+
 def test(arg):
 	return ((arg**3)*1.0) + (3.0*arg) - (1*1.0)
+
+def test_prime(arg):
+	return (3*(arg**2)*1.0) + (3.0) 
 
 def test2(arg):
 	return ((arg**3)*1.0) - (2.0*math.sin(arg))
@@ -11,18 +16,12 @@ def test2(arg):
 def test3(arg):
 	return (1.0*arg) + 10 - ((1.0*arg) * math.cos(50/(1.0*arg)))
 
-def Bisection(_function,a,b, nmax, epsilon):
-	# n = 0
-	# c = 0.0
-	# fa = 0.0
-	# fb = 0.0 
-	# fc = 0.0
-	error = 0.0
+def Bisection(_function,a,b, nmax):
 	fa = _function(a)
 	fb = _function(b)
 	if sign(fa,fb):
 		#output a,b, fa,fb
-		print("a : %5.2f, b : %5.2f, fa : %5.2f, fb : %5.2f" % (a,b,fa,fb))
+		print("a : %20.18f, b : %20.18f, fa : %20.18f, fb : %20.18f" % (a,b,fa,fb))
 		print("Inadequate values for a and b.\n")
 		return -1
 	#endif
@@ -33,7 +32,7 @@ def Bisection(_function,a,b, nmax, epsilon):
 		fc = _function(c)
 		print("n : %5d, c : %20.18f, fc : %20.18f, error : %20.18f" % (n,c,fc,error))
 		if (abs(error) < epsilon) or (fc == 0.0):
-			print("Algorithm has converged after %d iterations!\n" % n)
+			print("Bisection algorithm has converged after %d iterations!\n" % n)
 			return c
 		if sign(fa, fc) == False:
 			b = c
@@ -41,23 +40,19 @@ def Bisection(_function,a,b, nmax, epsilon):
 		else:
 			a = c
 			fa = fc
-	print ("Max iterations reached without convergence...\n")
+	print ("Max iterations reached without convergence in Bisection method...\n")
 	return c
 #end Bisection
 
 def sign(x, y):
 	return x * y > 0
 
-Bisection(test2,0.5,2, 54, eps)
 
 
-def Newton(_func, deriv_func, nmax, epsilon, delta):
-	# n = 0
-	# x = 0.0
-	# fx = 0.0
-	# fp = 0.0
+
+def Newton(_func, deriv_func, x, nmax):
 	fx = _func(x)
-	print("0, x: %5.2f, fx: %5.2f" % (x, fx))
+	print("0, x: %20.18f, fx: %20.18f" % (x, fx))
 	for n in range(nmax):
 		fp = deriv_func(x)
 		if (abs(fp) < delta):
@@ -66,20 +61,15 @@ def Newton(_func, deriv_func, nmax, epsilon, delta):
 		d = fx / fp
 		x -= d
 		fx = _func(x)
-		print("n: %d, x: %5.2f, fx: %5.2f" % (n, x, fx))
+		print("n: %d, x: %20.18f, fx: %20.18f" % (n, x, fx))
 		if (abs(d) < epsilon):
-			print("Algorithm has converged after %d iterations!" % (n))
+			print("Newton algorithm has converged after %d iterations!" % (n))
 			return x
 #end Newton
 
-def Secant(_func, a, b, nmax, epsilon):
-	# n = 0
-	# fa = 0.0
-	# fb = 0.0
-	# d = 0.0
+def Secant(_func, a, b, nmax):
 	fa = _func(a)
 	fb = _func(b)
-
 	if (abs(fa) > abs(fb)):
 		temp = a
 		a = b 
@@ -87,8 +77,8 @@ def Secant(_func, a, b, nmax, epsilon):
 		temp = fa
 		fa = fb 
 		fb = temp
-	print("0, a: %5.2f, fa: %5.2f" % (a, fa))
-	print("1, b: %5.2f, fb: %5.2f" % (b, fb))
+	print("0, a: %20.18f, fa: %20.18f" % (a, fa))
+	print("1, b: %20.18f, fb: %20.18f" % (b, fb))
 	for n in range(1, nmax):
 		if (abs(fa) > abs(fb)):
 			temp = a
@@ -102,17 +92,18 @@ def Secant(_func, a, b, nmax, epsilon):
 		fb = fa 
 		d *= fa
 		if (abs(d) < epsilon):
-			print("Algorithm has converged after %d iterations!" % (n))
+			print("Secant algorithm has converged after %d iterations!" % (n))
 			return a
 		a = a - d
 		fa = _func(a)
-		print("n: %d, a: %5.2f, fa: %5.2f" % (n, a, fa))
-	print ("Max iterations reached without convergence...\n")
+		print("n: %d, a: %20.18f, fa: %20.18f" % (n, a, fa))
+	print ("Max iterations reached without convergence using Secant method...\n")
 	return a
 #end Secant
 
 
-
-
+Bisection(test2,0.5,2,54)
+Newton(test, test_prime,0.5,54)
+Secant(test2,0.5,2,54)
 
 
